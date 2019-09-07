863c1136d0dee3e6b93ac6b165d1ceed9e7021ab
nikshevtsov6 <nikshevtsov6@gmail.com>
2019-09-07 19:23:46 +0300

    upload App1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello man</p>
+        <p><span>Hello man</span>Hello man</p>
         <a
           className="App-link"
           href="https://reactjs.org"
